33dd170a02e1cc2ae191e080dc65946c3a53b2d7
Joseph Murray <josephl.murrayjr@gmail.com>
2022-06-03 23:06:43 -0400

    deletions and corrections for deployment

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -15,10 +15,10 @@ const SignupForm = () => {
   // set state for form validation
   const [validated] = useState(false);
 
-  const [addUser, { error }] = useMutation(ADD_USER);
-
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
