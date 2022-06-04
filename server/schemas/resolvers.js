7005f97ff503f0f8962fe124821f606bb007d758
Joseph Murray <josephl.murrayjr@gmail.com>
2022-06-03 21:18:36 -0400

    first query and mutations in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,41 @@
-const {} = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 
-  Mutation: {},
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
